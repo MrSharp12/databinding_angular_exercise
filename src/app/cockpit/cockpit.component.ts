ae75f52aece3dc5a91444e3878d6a6991f396d2e
The Sharps <Sharp@Thes-MacBook-Air.local>
2018-03-08 08:20:39 -0500

    added alias to custom event

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -18,7 +18,7 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 //we're passing our own event out of the component
 export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   newServerName = '';
   newServerContent = '';
 
